c6f12bba21752f347201353e1b7f162dec951ea7
Thais Silveira <thais_lsilveira@hotmail.com>
2019-10-02 23:50:50 -0300

    interface web - finalizada

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-uxphp.mongodb.net/
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);

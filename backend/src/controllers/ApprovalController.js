2d19ba35b323856d6fe37d952b248a8e71d6cd13
Thais Silveira <thais_lsilveira@hotmail.com>
2019-10-04 17:12:37 -0300

    Funcionalidades avançadas - mobile

--- backend/src/controllers/ApprovalController.js
+++ backend/src/controllers/ApprovalController.js
@@ -12,6 +12,12 @@ module.exports = {
 
         await booking.save();
 
+        const bookingUserSocket = req.connectedUsers[booking.user];
+
+        if(bookingUserSocket){
+            req.io.to(bookingUserSocket).emit('booking_response', booking)
+        }
+
         return res.json(booking);
     }
 };
